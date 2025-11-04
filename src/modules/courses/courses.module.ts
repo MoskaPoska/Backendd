8cd12916caf551922788d14893d48e9033d3ac20
MuadDib <artem.buravlev333@gmail.com>
2025-11-05 01:44:49 +0200

    Исправил ошибку при подключени в azure

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -4,11 +4,13 @@ import { Course } from '../../entities/course.entity';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { AuthModule } from '../auth/auth.module';
+import {AzureModule} from "../../azure/azure.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Course]),
         AuthModule,
+        AzureModule,
     ],
     providers: [CoursesService],
     controllers: [CoursesController],
